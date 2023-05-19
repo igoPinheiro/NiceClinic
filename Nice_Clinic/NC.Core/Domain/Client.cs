928a405e898236c980e098aa85ec70e7dfbd1cd5
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-05-19 18:00:56 -0400

    Create database and objects using EF, migrations initial

--- Nice_Clinic/NC.Core/Domain/Client.cs
+++ Nice_Clinic/NC.Core/Domain/Client.cs
@@ -1,15 +1,19 @@
-﻿namespace NC.Core.Domain;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NC.Core.Domain;
 
 public class Client
 {
+    [Required]
     public int Id { get; set; }
-    public string? Name { get; set; }
+    [StringLength(100,MinimumLength =3,ErrorMessage ="Nome deve ter entre 3 e 100 caracteres")]
+    public string? Name { get; set; }    
     public DateTime BirthDate { get; set; }
    // public Sexo Sexo { get; set; }
     //public ICollection<Phone> Phones { get; set; }
-    public string? Document { get; set; }
-    public DateTime CreationDate { get; set; }
-    public DateTime? LastUpdate { get; set; }
+    //public string? Document { get; set; }
+    //public DateTime CreationDate { get; set; }
+    //public DateTime? LastUpdate { get; set; }
 
    // public Endereco Endereco { get; set; }
 }
