4cd45c91f467c50e2cdee9fabdeb2e39ecdb954c
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-05-24 10:31:52 -0400

    Add Geral Info Swagger Doc

--- Nice_Clinic/NC.WebApi/Configuration/SwaggerConfig.cs
+++ Nice_Clinic/NC.WebApi/Configuration/SwaggerConfig.cs
@@ -8,7 +8,26 @@ public static class SwaggerConfig
     {
         services.AddSwaggerGen(c =>
         {
-            c.SwaggerDoc("v1", new OpenApiInfo { Title = "Consultório Legal", Version = "v1" });
+            c.SwaggerDoc("v1",
+                new OpenApiInfo
+                {
+                    Title = "Consultório Legal",
+                    Version = "v1",
+                    Description = "API da aplicação consultório legal",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Igo Pinheiro",
+                        Email = "igo.pinheiro1@gmail.com",
+                        Url = new Uri("https://github.com/igoPinheiro")
+                    },
+                    License = new OpenApiLicense
+                    {
+                        Name = "OSD",
+                        Url = new Uri("https://github.com/igoPinheiro/NiceClinic")
+                    },
+                    TermsOfService = new Uri("https://github.com/igoPinheiro/NiceClinic")
+
+                });
         });
     }
 
