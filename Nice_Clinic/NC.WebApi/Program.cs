4e69359d921d2adcff2888e998f47960ef56aa9a
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-05-24 19:08:59 -0400

    Log application logs and save them to a file
    - install-package serilog.aspnetcore
    - install-package serilog.sinks.file

--- Nice_Clinic/NC.WebApi/Program.cs
+++ Nice_Clinic/NC.WebApi/Program.cs
@@ -1,39 +1,66 @@
 using NC.WebApi.Configuration;
+using Serilog;
 
-var builder = WebApplication.CreateBuilder(args);
+Log.Logger = new LoggerConfiguration()
+    .Enrich.FromLogContext()
+    .WriteTo.Console()
+    //.WriteTo.File("log.txt",fileSizeLimitBytes: 100000) // limitando a 100KB. Quando atingir 100Kb, ele apaga os log mais antigos, para dar espaçõ aos logs mais novos. 
+  //  .WriteTo.File("log.txt", fileSizeLimitBytes: 100000, rollOnFileSizeLimit:true) // Cria um novo arquivo a casa 100KB
+    .WriteTo.File("log.txt", fileSizeLimitBytes: 100000, rollOnFileSizeLimit: true, rollingInterval: RollingInterval.Day) // Cria um arquivo por dia se esse arquivo nesse dia chegar a 100kb ele cria outro
+    .CreateLogger();
 
-// Add services to the container.
-builder.Services.AddControllers();
 
-builder.Services.AddFluentValidationConfiguration();
-
-builder.Services.AddAutoMapperConfiguration();
-
-builder.Services.AddDatabaseConfiguration(builder.Configuration);
-
-AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
-
-builder.Services.AddDependencyInjectionConfiguration();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-
-builder.Services.AddSwaggerConfiguration();
-
-var app = builder.Build();
-
-app.UseDatabaseConfiguration();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+try
 {
-    app.UseSwaggerConfiguration();
+    Log.Information("Iniciando Web Api");
+
+    var builder = WebApplication.CreateBuilder(args);
+
+    builder.Host.UseSerilog();
+
+
+    // Add services to the container.
+    builder.Services.AddControllers();
+
+    builder.Services.AddFluentValidationConfiguration();
+
+    builder.Services.AddAutoMapperConfiguration();
+
+    builder.Services.AddDatabaseConfiguration(builder.Configuration);
+
+    AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+    builder.Services.AddDependencyInjectionConfiguration();
+
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+
+    builder.Services.AddSwaggerConfiguration();
+
+    var app = builder.Build();
+
+    app.UseDatabaseConfiguration();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwaggerConfiguration();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseAuthorization();
+
+    app.MapControllers();
+
+    app.Run();
 }
+catch (Exception ex)
+{
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
+    Log.Fatal(ex, "Erro Catastrofico");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
