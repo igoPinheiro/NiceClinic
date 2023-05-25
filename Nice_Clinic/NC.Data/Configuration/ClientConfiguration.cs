cc0c5d354d114c92f96b8a243becef700108df13
Igo Pinheiro <igo.pinheiro1@gmail.com>
2023-05-25 17:23:07 -0400

    Add configuration with Fluent Api

---
+++ Nice_Clinic/NC.Data/Configuration/ClientConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NC.Core.Domain;
+
+namespace NC.Data.Configuration;
+
+public class ClientConfiguration : IEntityTypeConfiguration<Client>
+{
+    public void Configure(EntityTypeBuilder<Client> builder)
+    {
+        builder.HasKey(x => x.Id);
+        //builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+        //builder.Property(p => p.Sexo).HasDefaultValue("M").IsRequired();
+
+        builder.HasIndex(x => new { x.Name, x.Sexo });
+    }
+}
